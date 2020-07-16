9bacae80916e662c6ad067b3a2ab427f1fbd5909
Ben Eich <jgradio64@yahoo.com>
2020-07-15 23:45:41 -0400

    update edit template

--- views/admin/products/edit.js
+++ views/admin/products/edit.js
@@ -6,28 +6,28 @@ module.exports = ({ errors, product }) => {
 		content: `
         <div class="columns is-centered">
             <div class="column is-half">
-            <h1 class="subtitle">Edit a Product</h1>
+                <h1 class="subtitle">Edit a Product</h1>
 
-            <form method="POST" enctype="multipart/form-data">
-                <div class="field">
-                <label class="label">Title</label>
-                <input class="input" value="${product.title}" name="title">
-                <p class="help is-danger">${getError(errors, 'title')}</p>
-                </div>
-                
-                <div class="field">
-                <label class="label">Price</label>
-                <input class="input" value="${product.price}" name="price">
-                <p class="help is-danger">${getError(errors, 'price')}</p>
-                </div>
-                
-                <div class="field">
-                <label class="label">Image</label>            
-                <input type="file" name="image">
-                </div>
-                <br />
-                <button class="button is-primary">Create</button>
-            </form>
+                <form method="POST" enctype="multipart/form-data">
+                    <div class="field">
+                        <label class="label">Title</label>
+                        <input class="input" value="${product.title}" name="title">
+                        <p class="help is-danger">${getError(errors, 'title')}</p>
+                    </div>
+                    
+                    <div class="field">
+                        <label class="label">Price</label>
+                        <input class="input" value="${product.price}" name="price">
+                        <p class="help is-danger">${getError(errors, 'price')}</p>
+                    </div>
+                    
+                    <div class="field">
+                        <label class="label">Image</label>            
+                        <input type="file" name="image">
+                    </div>
+                    <br />
+                    <button class="button is-primary">Create</button>
+                </form>
             </div>
         </div>
         `
