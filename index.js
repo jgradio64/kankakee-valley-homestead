3d786a4b54a01947b63d83a3946aeacdeb7de36d
Ben Eich <jgradio64@yahoo.com>
2020-10-06 13:23:03 -0400

    Price was appearing without trailing decimal zeros

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
 	cookieSession({
-		keys: [ 'aqu7whvj4cx876wowdk132' ]
+		keys: [ "aqu7whvj4cx876wowdk132" ]
 	})
 );
 app.use(authRouter);
